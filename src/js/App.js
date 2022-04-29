b328cd2d1c564c4e3ec2065d00c52037bf896a66
AtakanDonmez <atk.donmez@gmail.com>
2022-04-29 19:04:46 +0300

    merge didnt work

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link} from "react-router-dom";
+import SelectFace from "./SelectFace";
 
 export default function App(){
     return(
@@ -16,9 +17,7 @@ export default function App(){
             <button onClick={() =>
                 electron.modelsApi.boundaryBox()
             }>Draw Boundary Box</button>
-            <button onClick={() => {
-                drawBoundaries()
-            }}>Draw2</button>
+            <SelectFace/>
             <button onClick={() =>
                 electron.nextPageApi.nextPage()
             }>Next Page</button>
@@ -32,48 +31,5 @@ export default function App(){
     )
 }
 
-function drawBoundaries(){
-    var _target = document.getElementById('image_container');
-    var _in = _target.innerHTML;
 
-    return(
-        <div style={styles.container}>
-            {_in}
-            <div style={styles.boxContainer}>
-                <button style={styles.boxForeground}></button>
-            </div>
-        </div>);
-}
 
-const styles = {
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'dark',
-    },
-    image: {
-        flex: 1,
-        width: 500,
-        height: undefined,
-        resizeMode: 'contain',
-    },
-    boxContainer: {
-        position: 'absolute',
-        backgroundColor: 'transparent',
-        width: 500,
-        height: 500,
-    },
-    boxForeground: {
-        position: 'absolute',
-        borderColor: 'green',
-        backgroundColor: 'green',
-        borderWidth: 2,
-        borderRadius: 4,
-        opacity: 0.3,
-        height: 50,
-        width: 50,
-        top: 50,
-        left: 50,
-    },
-};
---
+++ src/js/SelectFace.js
@@ -0,0 +1,51 @@
+import FaceBox from "../js/FaceBox";
+import path from "path";
+
+export default function SelectFace(){
+/*    var _target = document.getElementById('image_container');
+    var _in = _target.innerHTML;*/
+    var filepath = "C:\\Users\\atkdo\\Desktop\\okul\\4 - 2\\CS 492\\desktop\\electron-react-app\\assets\\image1.jpg";
+    var extension = path.extname(filepath);
+    //const image = '<img src="data:image/' + extension + ';base64,' + filepath + '" />';
+    const imageSrc = "data:image/" + extension + ";base," + filepath
+    return(
+        <div style={styles.container}>
+            <img src={imageSrc} style={styles.image}/>
+            <div style={styles.boxContainer}>
+                <button style={styles.boxForeground}></button>
+            </div>
+        </div>);
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'dark',
+    },
+    image: {
+        flex: 1,
+        width: 500,
+        height: undefined,
+        resizeMode: 'contain',
+    },
+    boxContainer: {
+        position: 'absolute',
+        backgroundColor: 'transparent',
+        width: 500,
+        height: 500,
+    },
+    boxForeground: {
+        position: 'absolute',
+        borderColor: 'green',
+        backgroundColor: 'green',
+        borderWidth: 2,
+        borderRadius: 4,
+        opacity: 0.3,
+        height: 50,
+        width: 50,
+        top: 50,
+        left: 50,
+    },
+};
